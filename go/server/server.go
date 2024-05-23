5bc76c25ce821edf36802db628dfd39945e7a22d
Hélisson Magalhães <helissonmagalhaes@hotmail.com>
2024-05-23 23:44:20 +0100

    .

--- go/server/server.go
+++ go/server/server.go
@@ -6,11 +6,21 @@ import (
 	"go-django/middleware"
 	"log"
 	"net/http"
+    "time"
 	"os"
-
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
+
+func tableExists(db *sql.DB, tableName string) (bool, error) {
+    query := fmt.Sprintf("SHOW TABLES LIKE '%s';", tableName)
+    var name string
+    err := db.QueryRow(query).Scan(&name)
+    if err != nil && err != sql.ErrNoRows {
+        return false, err
+    }
+    return name == tableName, nil
+}
 
 func main() {
 	r := mux.NewRouter()
@@ -23,7 +33,7 @@ func main() {
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
 	// Open a connection to the database
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
@@ -36,9 +46,89 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error connecting to the database: %v", err)
 	}
-
 	log.Printf("Connected to the database successfully!")
+    { // Create a new table
+        exists, err := tableExists(db, "users")
+        if err != nil {
+            panic(err)
+        }
+        if !exists {
+            query := `
+                CREATE TABLE users (
+                    id INT AUTO_INCREMENT,
+                    username TEXT NOT NULL,
+                    password TEXT NOT NULL,
+                    created_at DATETIME,
+                    PRIMARY KEY (id)
+                );`
 
+            if _, err := db.Exec(query); err != nil {
+                panic(err)
+            }
+            fmt.Println("Table created successfully.")
+        }else {
+            fmt.Println("Table already exists.")
+        }
+    }
+    { // Insert a new user
+        username := "johndoe"
+        password := "secret"
+        createdAt := time.Now()
+
+        result, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        id, err := result.LastInsertId()
+        fmt.Println(id)
+    }
+
+    { // Query a single user
+        var (
+            id        int
+            username  string
+            password  string
+            createdAt time.Time
+        )
+
+        query := "SELECT id, username, password, created_at FROM users WHERE id = ?"
+        if err := db.QueryRow(query, 1).Scan(&id, &username, &password, &createdAt); err != nil {
+            log.Fatal(err)
+        }
+
+        fmt.Println(id, username, password, createdAt)
+    }
+    { // Query all users
+        type user struct {
+            id        int
+            username  string
+            password  string
+            createdAt time.Time
+        }
+
+        rows, err := db.Query(`SELECT id, username, password, created_at FROM users`)
+        if err != nil {
+            log.Fatal(err)
+        }
+        defer rows.Close()
+
+        var users []user
+        for rows.Next() {
+            var u user
+
+            err := rows.Scan(&u.id, &u.username, &u.password, &u.createdAt)
+            if err != nil {
+                log.Fatal(err)
+            }
+            users = append(users, u)
+        }
+        if err := rows.Err(); err != nil {
+            log.Fatal(err)
+        }
+
+        log.Printf("%v", users[0].createdAt)
+    }
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
 	})
